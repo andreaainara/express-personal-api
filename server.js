bb1073d7bd61883f8917e27965b330ebc495f6c8
andreaainara <andrea.ainara10@gmail.com>
2016-04-03 14:34:47 -0700

    server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
  * DATABASE *
  ************/
 
-// var db = require('./models');
+var db = require('./models');
 
 /**********
  * ROUTES *
@@ -20,6 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Serve static files from the `/public` directory:
 // i.e. `/images`, `/scripts`, `/styles`
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /*
  * HTML Endpoints
@@ -34,13 +35,20 @@ app.get('/', function homepage(req, res) {
  * JSON API Endpoints
  */
 
-app.get('/api', function api_index(req, res) {
+app.get('/api/profile', function api_index(req, res) {
   // TODO: Document all your api endpoints below
+  db.Andrea.find().populate('andrea')
+    .exec(fucntion(err, andrea) {
+      if (err) { return console.log("index error:" + err); }
+      res.json(andrea);
+    });
+  });
+
   res.json({
-    woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
+    // woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentation_url: "https://github.com/andreaainara/express_self_api/README.md", // CHANGE ME
+    base_url: "https://pumpkin-sundae-78037.herokuapp.com/", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
